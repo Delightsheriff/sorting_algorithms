a6bb50a0370db842c69ad6c2f257d6b3632ad938
Delight Sheriff <delightsheriff@gmail.com>
2023-07-24 13:06:20 +0100

    0. Bubble sort-2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 
 	if (!array)
 		return;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		swapped = 0;
 		for (j = 0; j < (size - i - 1); j++)
